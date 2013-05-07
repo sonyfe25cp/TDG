9c8fd359281adca0549d662f5d0f2b875e2001ab
sonyfe25cp@gmail.com <sonyfe25cp@gmail.com>
2013-05-07 18:00:56 +0800

    init

---
+++ src/com/omartech/tdg/model/Order.java
@@ -0,0 +1,22 @@
+package com.omartech.tdg.model;
+
+public class Order {
+
+	private long id;
+	private String comment;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+}
